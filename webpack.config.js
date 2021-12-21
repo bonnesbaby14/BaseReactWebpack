4075a8d52e13eee63755fd947bb8d7b754384181
bonnesbaby14 <bonnesbaby14@gmail.com>
2021-12-20 22:43:17 -0600

    se añadio lo nexesario para integrar css y scss

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path =require("path");
-const HtmlWebpackPlugin=require("html-webpack-plugin")
+const HtmlWebpackPlugin=require("html-webpack-plugin");
+const MiniCss =require("mini-css-extract-plugin");
 module.exports={
     entry:"./index.js",
     mode :"development",
@@ -24,7 +25,17 @@ module.exports={
                 use:[
                     {loader:"html-loader"}
                 ]
+            },
+            {
+                test:/\.s[ac]ss$/,
+                use:[
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader"
+                ]
+
             }
+        
         ]
     },
     plugins:[
@@ -32,6 +43,9 @@ module.exports={
             template:"./public/index.html",
             filename:"./index.html"
             
+        }),
+        new MiniCss({
+            filename:"[name].css"
         })
     ],
 
